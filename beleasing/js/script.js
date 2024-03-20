e67ea44315d1006ecb17aed6b41ecac93eb66583
imanajjari <iman.najjri9494@gmail.com>
2024-03-20 15:42:15 +0330

    create nav adn footer

---
+++ beleasing/js/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var ul = document.getElementById("nav_ul");
+    if (ul.style.display === "block") {
+        ul.style.display = "none";
+    } else {
+        ul.style.display = "block";
+    }
+}
